c8911e38bc75e4198cf6650c6ab60d438b419e90
Albacrucexx <acrucesu@alumnos.nebrija.es>
2026-03-09 07:36:32 -0400

    Parcial programación deinterfaces web

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,24 +7,18 @@ import CocktailCard from "./components/cocktail";
 import { useRouter } from "next/navigation";
 
 const Home = () => {
-  const [search, setSearch] = useState("");
   const [cocktails, setCocktails] = useState<Cocktail[]>([]);
   const router = useRouter();
 
-  // CARGAR MARGARITAS AL INICIO
+  // cargar margaritas al inicio
   useEffect(() => {
-    const loadInitialCocktails = async () => {
+    const loadCocktails = async () => {
       const data = await searchCocktailByName("margarita");
       setCocktails(data || []);
     };
 
-    loadInitialCocktails();
+    loadCocktails();
   }, []);
-
-  const handleSearch = async () => {
-    const data = await searchCocktailByName(search);
-    setCocktails(data || []);
-  };
 
   const handleRandom = async () => {
     const cocktail = await getRandomCocktail();
@@ -36,19 +30,6 @@ const Home = () => {
       <h1>Buscador de Cocktails</h1>
 
       <div className="searchContainer">
-        <div className="searchBox">
-          <input
-            type="text"
-            placeholder="Buscar cocktail..."
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-          />
-
-          <button className="searchBtn" onClick={handleSearch}>
-            Buscar
-          </button>
-        </div>
-
         <button className="randomBtn" onClick={handleRandom}>
           Dime algo bonito
         </button>
